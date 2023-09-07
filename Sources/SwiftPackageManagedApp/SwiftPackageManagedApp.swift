a3cc5310ac33c5e865f292d0abf5085df97a7bcf
hiroya-hinomori <hiroya.hinomori@kinto-technologies.com>
2023-09-07 13:39:37 +0900

    Add: project file

---
+++ Sources/SwiftPackageManagedApp/SwiftPackageManagedApp.swift
@@ -0,0 +1,6 @@
+public struct SwiftPackageManagedApp {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/SwiftPackageManagedAppTests/SwiftPackageManagedAppTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftPackageManagedApp
+
+final class SwiftPackageManagedAppTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftPackageManagedApp().text, "Hello, World!")
+    }
+}
